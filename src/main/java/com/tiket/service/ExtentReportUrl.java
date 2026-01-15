1f783b7ea186eb9834b138d568627a3afab1c3aa
Areeb Malik <areeb.malik@tiket.com>
2026-01-16 02:06:17 +0530

    Update ExtentReportUrl.java

--- src/main/java/com/tiket/service/ExtentReportUrl.java
+++ src/main/java/com/tiket/service/ExtentReportUrl.java
@@ -3,7 +3,7 @@ package com.tiket.service;
 public class ExtentReportUrl {
     public static String get() {
         if(isJenkinsSystem()) {
-            String buildUrl = System.getenv("BUILD_URL");
+            String buildUrl = System.getProperty("BUILD_URL");
             return buildUrl + "artifact/extent-reports/extent-report.html";
         }
         return "Executed locally";
