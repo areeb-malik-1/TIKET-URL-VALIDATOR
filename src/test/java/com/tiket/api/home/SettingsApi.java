b9266a0a5aedfdf27d7f47aabd9683c1e6a47565
Areeb Malik <areeb.malik@tiket.com>
2026-01-16 00:30:45 +0530

    update

--- src/test/java/com/tiket/api/home/SettingsApi.java
+++ src/test/java/com/tiket/api/home/SettingsApi.java
@@ -1,4 +1,4 @@
-package com.tiket.api.flight;
+package com.tiket.api.home;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -10,7 +10,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-public class FlightApi implements BaseApi {
+public class SettingsApi implements BaseApi {
 
     private final String accessToken;
     private final String platform;
@@ -18,7 +18,7 @@ public class FlightApi implements BaseApi {
     private final ObjectMapper mapper = new ObjectMapper();
     private final HttpClient client = HttpClient.newHttpClient();
 
-    public FlightApi(String accessToken, String platform, String baseUrl) {
+    public SettingsApi(String accessToken, String platform, String baseUrl) {
         this.accessToken = accessToken;
         this.platform = platform;
         this.baseUrl = baseUrl;
@@ -26,49 +26,38 @@ public class FlightApi implements BaseApi {
 
     @Override
     public ApiResult hitApi() throws Exception {
-        String endpoint = "/ms-gateway/tix-flight-master-discovery/general-content-search/v3/find";
+        String endpoint = "/ms-gateway/tix-general-config/settings";
         String url = baseUrl + endpoint;
-        String fullUrl = url + "";
-        String jsonBody = """
-            {
-              "page": "LANDING_PAGE"
-            }
-            """;
+        String fullUrl = url + "?language=en";
 
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(fullUrl))
                 .header("authorization", "Bearer " + this.accessToken)
-                .header("containername",
-                        "com.tiket.android.flight.presentation.landing.FlightLandingActivity")
-                .header("screenname",
-                        "com.tiket.android.flight.presentation.landing.FlightLandingActivity")
-                .header("x-correlation-id",
-                        "c5f0154e-aa0b-481a-a5c7-ad9d5b3cc3ca|1767870859160")
+                .header("containername", "com.tiket.gits.HomeSplashActivity")
+                .header("screenname", "com.tiket.gits.HomeSplashActivity")
+                .header("x-correlation-id", "33988b61-52f8-43ad-af66-447ff2ef4642|1768377027833")
                 .header("deviceid", "179dd086888c94ec")
                 .header("devicemodel", "Xiaomi+23108RN04Y")
                 .header("osversion", "14")
-                .header("appversion",
-                        "5.9.1-debug-NCT-20474/play_integrity")
-                .header("tiketsessionid",
-                        "c5f0154e-aa0b-481a-a5c7-ad9d5b3cc3ca")
+                .header("appversion", "5.9.1-uat-HEAD")
+                .header("tiketsessionid", "33988b61-52f8-43ad-af66-447ff2ef4642")
                 .header("platform", "ANDROID")
-                .header("tiket-user-agent",
-                        "tiketcom/android-version (twh:20296642) - v5.9.1-debug-NCT-20474/play_integrity")
+                .header("tiket-user-agent", "tiketcom/android-version (twh:20296642) - v5.9.1-uat-HEAD")
                 .header("lang", "en")
-                .header("currency", "SGD")
+                .header("currency", "USD")
                 .header("accept-language", "en")
-                .header("x-currency", "SGD")
+                .header("x-currency", "USD")
                 .header("x-country-code", "IDN")
                 .header("language", "en")
+                .header("content-type", "application/json")
                 .header("channelid", "ANDROID")
-                .header("Content-Type", "application/json; charset=UTF-8")
                 .header("Accept-Encoding", "gzip")
-                .POST(HttpRequest.BodyPublishers.ofString(jsonBody))
+                .GET()
                 .build();
 
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        System.out.println("\n=== Flight API Response ===");
+        System.out.println("\n=== Settings API Response ===");
         System.out.println("Status: " + response.statusCode());
 
         JsonNode data = null;
