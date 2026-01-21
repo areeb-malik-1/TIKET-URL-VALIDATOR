4c09f20631b99d2d99d82c63f521400b0aac0d3c
Areeb Malik <areeb.malik@tiket.com>
2026-01-21 10:27:51 +0530

    Update BaseApi.java

--- src/main/java/com/tiket/testbase/BaseApi.java
+++ src/main/java/com/tiket/testbase/BaseApi.java
@@ -2,12 +2,10 @@ package com.tiket.testbase;
 
 import com.tiket.model.ApiResult;
 
-import static org.hamcrest.core.Is.is;
-
 public interface BaseApi {
     ApiResult hitApi() throws Exception;
 
     default void isSuccess(int statusCode) {
-        Assertion.assertThat("Api statusCode", statusCode >= 200 && statusCode < 300, is(true));
+        assert (statusCode >= 200 && statusCode < 300);
     }
 }
