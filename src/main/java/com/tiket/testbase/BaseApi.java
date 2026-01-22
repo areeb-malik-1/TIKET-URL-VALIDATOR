6b4d04ca29d2e156b9bdbcaab47ec3810af56200
Areeb Malik <areeb.malik@tiket.com>
2026-01-22 14:50:46 +0530

    Update BaseApi.java

--- src/main/java/com/tiket/testbase/BaseApi.java
+++ src/main/java/com/tiket/testbase/BaseApi.java
@@ -2,15 +2,22 @@ package com.tiket.testbase;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.tiket.model.ApiResult;
+import org.apache.logging.log4j.Logger;
 
 import java.net.http.HttpResponse;
 
 public interface BaseApi {
+
+    Logger logger = org.apache.logging.log4j.LogManager.getLogger(BaseApi.class);
+
     ApiResult hitApi() throws Exception;
 
     default void isSuccess(HttpResponse<?> response, JsonNode data) {
         int statusCode = response.statusCode();
-        assert (statusCode >= 200 && statusCode < 300);
-        assert data != null;
+        boolean ok = statusCode >= 200 && statusCode < 300;
+        if(!ok || data == null) {
+            logger.debug("Base Api Failed, Response Status Code: {}, for API: {}, requestUrl: {}", statusCode, this.getClass().getSimpleName(), response.request().uri());
+        }
+        assert ok && data != null;
     }
 }
