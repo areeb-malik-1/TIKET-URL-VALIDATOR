d533fcf8bb577beec10ea00d1c9776a0dee4f6a6
Areeb Malik <areeb.malik@tiket.com>
2026-01-23 15:17:51 +0530

    Update ExtentTestManager.java

--- src/main/java/com/tiket/report/ExtentTestManager.java
+++ src/main/java/com/tiket/report/ExtentTestManager.java
@@ -13,15 +13,15 @@ public class ExtentTestManager {
 
     private static final Logger logger = LogManager.getLogger(ExtentTestManager.class);
     private static final ExtentReports extentReports = ExtentReportManager.getReports();
-    private static final ThreadLocal<Map<String, ExtentTest>> nameToTestMap = ThreadLocal.withInitial(HashMap::new);
+    private static final Map<String, ExtentTest> nameToTestMap = new ConcurrentHashMap<>();
 
     public synchronized static ExtentTest getTest(String testName, String testDescription, long timestamp) {
         String name = testName + "_" + timestamp;
         ExtentTest test = extentReports.createTest(name, testDescription);
-        if (!nameToTestMap.get().containsKey(name)) {
+        if (!nameToTestMap.containsKey(name)) {
             try {
                 Long threadID = Thread.currentThread().getId();
-                nameToTestMap.get().put(name, test);
+                nameToTestMap.put(name, test);
                 TestCountTracker.incrementTestsInExtentReport(testName);
                 logger.info("Created ExtentTest for: {} on thread: {}", name, threadID);
             } catch (Exception e) {
@@ -29,7 +29,7 @@ public class ExtentTestManager {
                 throw new RuntimeException("Failed to create test: " + name, e);
             }
         }
-        return nameToTestMap.get().get(name);
+        return nameToTestMap.get(name);
     }
 
     public synchronized static ExtentTest getTest(String testName, long timestamp) {
