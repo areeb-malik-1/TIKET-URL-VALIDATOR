28eab76c68c32eec361817fa435f617df0634428
Areeb Malik <areeb.malik@tiket.com>
2026-02-02 14:08:59 +0530

    Update AnnotationTransformer.java

--- src/main/java/com/tiket/testng/AnnotationTransformer.java
+++ src/main/java/com/tiket/testng/AnnotationTransformer.java
@@ -3,6 +3,8 @@ package com.tiket.testng;
 import com.tiket.annotation.Scope;
 import com.tiket.model.Platform;
 import com.tiket.testbase.Config;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.IAnnotationTransformer;
 import org.testng.annotations.ITestAnnotation;
 
@@ -11,14 +13,21 @@ import java.lang.reflect.Method;
 
 public class AnnotationTransformer implements IAnnotationTransformer {
 
+    Logger logger = LogManager.getLogger(getClass());
+
     @Override
     public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
-        Platform[] platforms = testMethod.getAnnotation(Scope.class).platforms();
-        for(Platform platform : platforms) {
-            if (platform == Config.PLATFORM) {
-                annotation.setEnabled(true);
-                return;
+        try {
+            Platform[] platforms = testMethod.getAnnotation(Scope.class).platforms();
+            for (Platform platform : platforms) {
+                if (platform == Config.PLATFORM) {
+                    annotation.setEnabled(true);
+                    return;
+                }
             }
+        } catch (Exception e) {
+            logger.error(e);
+            annotation.setEnabled(false);
         }
     }
 }
