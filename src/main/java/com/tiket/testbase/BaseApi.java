13d024e468cc3b584e00e6e2e13fcdf782f8e62d
Areeb Malik <areeb.malik@tiket.com>
2026-02-03 16:25:22 +0530

    Update BaseApi.java

--- src/main/java/com/tiket/testbase/BaseApi.java
+++ src/main/java/com/tiket/testbase/BaseApi.java
@@ -26,7 +26,7 @@ public interface BaseApi {
         int statusCode = response.statusCode();
         boolean ok = statusCode >= 200 && statusCode < 300;
         if(!ok || data == null) {
-            logger.debug("Base Api Failed, Response Status Code: {}, for API: {}, requestUrl: {}, is data null?: {}, curl: {}", statusCode, this.getClass().getSimpleName(), response.request().uri(), data == null, CurlBuilder.toCurl(request, response, requestBody));
+            logger.debug("Base Api Failed, Response Status Code: {}, for API: {}, requestUrl: {}, is data null?: {}, curl: {}, response: {}", statusCode, this.getClass().getSimpleName(), response.request().uri(), data == null, CurlBuilder.toCurl(request, response, requestBody), response.body());
             NON_WORKING_APIS.add(this.getClass().getSimpleName());
         }
         assert ok && data != null;
